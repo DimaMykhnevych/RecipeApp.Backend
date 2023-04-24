f5645d0b2d0616f15e54b213bf838e2dce968b92
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2023-04-25 01:22:19 +0300

    Added recipe nutrition population on recipe adding

--- RecipeApp.Web/RecipeApp.Application/Commands/Recipe/AddRecipe/AddRecipeCommandHandler.cs
+++ RecipeApp.Web/RecipeApp.Application/Commands/Recipe/AddRecipe/AddRecipeCommandHandler.cs
@@ -3,21 +3,25 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using RecipeApp.Domain.Entities;
 using RecipeApp.Domain.Repositories.RecipeRepository;
+using RecipeApp.Domain.Services.Recipe.AddRecipeNutritionService;
 
 namespace RecipeApp.Application.Commands.RecipeN.AddRecipe
 {
     public class AddRecipeCommandHandler : IRequestHandler<AddRecipeCommand, bool>
     {
         private readonly IRecipeRepository _recipeRepository;
+        private readonly IAddRecipeNutritionService _addRecipeNutritionService;
         private readonly IMapper _mapper;
         private readonly ILogger _logger;
 
         public AddRecipeCommandHandler(
             IRecipeRepository recipeRepository,
+            IAddRecipeNutritionService addRecipeNutritionService,
             IMapper mapper,
             ILoggerFactory loggerFactory)
         {
             _recipeRepository = recipeRepository;
+            _addRecipeNutritionService = addRecipeNutritionService;
             _mapper = mapper;
             _logger = loggerFactory?.CreateLogger(nameof(AddRecipeCommandHandler));
         }
@@ -31,8 +35,10 @@ namespace RecipeApp.Application.Commands.RecipeN.AddRecipe
             {
                 Recipe recipe = _mapper.Map<Recipe>(request.Recipe);
                 recipe.AppUserId = request.UserId;
-                await _recipeRepository.Insert(recipe);
+                Recipe addedRecipe = await _recipeRepository.Insert(recipe);
                 await _recipeRepository.Save();
+
+                await _addRecipeNutritionService.AddRecipeNutrition(addedRecipe.Id);
                 return true;
             }
             catch (Exception ex)
